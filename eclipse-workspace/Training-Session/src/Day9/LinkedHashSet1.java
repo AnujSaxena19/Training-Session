7a1e74db06aa6e6b61998842af6344f430305a8f
19anujsaxena19 <19anujsaxena19@gmail.com>
2021-10-19 00:06:29 +0530

    Collection Example

---
+++ eclipse-workspace/Training-Session/src/Day9/LinkedHashSet1.java
@@ -0,0 +1,16 @@
+package Day9;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSet1 {
+	public static void main(String args[]) {
+		LinkedHashSet h=new LinkedHashSet();
+		h.add(10);
+		h.add(20);
+		h.add(50);
+		h.add(40);
+		h.add(20);
+		
+		System.out.println(h);
+	}
+}
---
+++ eclipse-workspace/Training-Session/src/Day9/TreeSet2.java
@@ -0,0 +1,24 @@
+package Day9;
+
+import java.util.TreeSet;
+
+public class TreeSet2 {
+	public static void main(String args[]) {
+		TreeSet h=new TreeSet();
+		Integer i=new Integer(30);
+		Dog d=new Dog();
+		System.out.println(d);
+		h.add(i);
+		h.add(10);
+		h.add(20);
+		h.add(50);
+		h.add(20);
+		h.add(40);
+		
+		
+		System.out.println(h);
+	}
+}
+class Dog{
+	
+}
