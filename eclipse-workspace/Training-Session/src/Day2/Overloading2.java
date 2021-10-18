e79f191dfb2e4f8b3d94f5da083fa2c08df972ce
19anujsaxena19 <19anujsaxena19@gmail.com>
2021-10-18 12:01:52 +0530

    Updated

---
+++ eclipse-workspace/Training-Session/src/Day2/Overloading2.java
@@ -0,0 +1,19 @@
+//Case study 2 of overloading
+package Day2;
+
+public class Overloading2 {
+	public void m1(Object o) {
+		System.out.println("Object type");
+	}
+	public void m1(String s) {
+		System.out.println("String argument");
+	}
+	public static void main(String args[]) {
+		Overloading2 o=new Overloading2();
+		o.m1(new Object()); //object version
+		o.m1("Classroom"); //String version
+		o.m1(null); //null
+	}
+}
+
+//String child class of object
