4182f9fba2f44d5ff84eaec1f8e757d71142608b
19anujsaxena19 <19anujsaxena19@gmail.com>
2021-10-18 14:32:44 +0530

    Case study

--- eclipse-workspace/Training-Session/src/Day2/Overloading3.java
+++ eclipse-workspace/Training-Session/src/Day2/Overloading3.java
@@ -1,5 +1,24 @@
+//case study 3 of overloading : compare String with stringbuffer with null value 
 package Day2;
 
 public class Overloading3 {
-
+	public void m1(String s){
+		System.out.println("String Argument");
+	}
+	public void m1(StringBuffer sb) {
+		System.out.println("StringBuffer argument");
+	}
+	public static void main(String [] args) {
+		Overloading3 o=new Overloading3();
+		o.m1("Classroom");//string version
+		o.m1(new StringBuffer("Session"));//StringBuffer version
+		o.m1(null);
+	}
 }
+
+//String child class of object
+//Stringbuffer child class of object
+/* error : reference to m1 is ambiguous
+ * o.m1(null);
+ * both method m1(String) in Overloading3 and method m1(StringBuffer) in Overloading3 
+ * */
