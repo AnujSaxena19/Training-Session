6e1abd55144e34422c18be6e9e581e9019679ff7
19anujsaxena19 <19anujsaxena19@gmail.com>
2021-10-18 12:06:12 +0530

    Updated

--- eclipse-workspace/Training-Session/src/Day7/Exercise14.java
+++ eclipse-workspace/Training-Session/src/Day7/Exercise14.java
@@ -1,6 +1,13 @@
 //Write a  program to get seconds since <year>.
 package Day7;
-
+import java.util.*;
 public class Exercise14 {
-
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the year");
+		int year=sc.nextInt();
+		int curr_year=2021;
+		int diff=curr_year-year;
+		System.out.println("The number of second from "+ year + " to present year is :- " +(diff*31536000));
+	}
 }
