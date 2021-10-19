78334f8dbea0b7aabe7f7e1772ee9fb7afa4e97d
19anujsaxena19 <19anujsaxena19@gmail.com>
2021-10-19 23:26:47 +0530

    PriorityQueue Example

---
+++ eclipse-workspace/Training-Session/src/Day10/PriorityQueue1.java
@@ -0,0 +1,17 @@
+package Day10;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueue1 {
+	public static void main(String args[]) {
+		PriorityQueue pq=new PriorityQueue();
+		
+		pq.offer(10);
+		pq.offer(20);
+		pq.offer(30);
+		pq.offer(40);
+		pq.offer(50);
+		
+		System.out.println(pq);
+	}
+}
